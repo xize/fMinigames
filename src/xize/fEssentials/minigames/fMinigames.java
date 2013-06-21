a08911f49776b2bf3fbe1a16a2250fb86782d8ad
xize <glucassen@live.nl>
2013-06-21 23:43:16 +0200

    create ctf config

--- src/xize/fEssentials/minigames/fMinigames.java
+++ src/xize/fEssentials/minigames/fMinigames.java
@@ -5,12 +5,16 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import xize.fEssentials.minigames.configuration.config;
+
 public class fMinigames extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
+	private config Config = new config(this);
 	
 	public void onEnable() {
 		if(Bukkit.getPluginManager().isPluginEnabled("fEssentials")) {
-			log.info("[fMiniGames] MiniGames plugin enabled!");	
+			log.info("[fMiniGames] MiniGames plugin enabled!");
+			Config.createConfigs();
 		} else {
 			log.info("[fMiniGames] The minigames plugin has been disabled, fEssentials core is needed to enable this plugin part!");
 			Bukkit.getPluginManager().disablePlugin(this);
